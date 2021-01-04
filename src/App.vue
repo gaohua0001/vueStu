13890aabcfd4146b6110d29b9cc3091154685a57
gaohua <gaohua@bjdvt.com>
2021-01-04 18:14:57 +0800

    44

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <template>
   <div id="nav">
-    <h1>哈哈</h1>
-
-    <el-row>
-  <el-button>默认按钮</el-button>
-  <el-button type="primary">主要按钮</el-button>
-  <el-button type="success">成功按钮</el-button>
-  <el-button type="info">信息按钮</el-button>
-  <el-button type="warning">警告按钮</el-button>
-  <el-button type="danger">危险按钮</el-button>
-</el-row>
 
   <router-view />
   </div>
