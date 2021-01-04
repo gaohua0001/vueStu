fdb8594aa874643ff06feb920f820284658a147a
gaohua <gaohua@bjdvt.com>
2021-01-04 16:26:12 +0800

    orign

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// 加载element组件库
+import ElementUI from 'element-ui'
+// 加载elemrnt组件库样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.config.productionTip = false
+// 全局注册element组件库
+Vue.use(ElementUI)
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
