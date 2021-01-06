09dcb5f2824ff6790b6c202d2b669d645a8cfe49
gaohua <gaohua@bjdvt.com>
2021-01-06 18:02:00 +0800

    66

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,50 @@
 <template>
-  <div class="login-container">登录页</div>
+  <div class="login-container">
+    <el-form ref="form" :model="user" label-width="80px">
+      <el-form-item label="手机号">
+        <el-input v-model="user.mobile"></el-input>
+      </el-form-item>
+      <el-form-item label="验证码">
+        <el-input v-model="user.code"></el-input>
+      </el-form-item>
+        <el-form-item>
+    <el-button type="primary" @click="onSubmit">登录</el-button>
+    <el-button>取消</el-button>
+  </el-form-item>
+</el-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+.login-container {
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url(./download.jpg) no-repeat;
+  background-size: contain;
+}
 </style>
