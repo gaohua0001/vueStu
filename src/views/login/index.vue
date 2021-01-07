f9605875d0c7b0125186f0d8e0d217af4096ead5
gaohua <gaohua@bjdvt.com>
2021-01-07 18:02:37 +0800

    77

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="login-container">
-    <el-form ref="form" :model="user" label-width="80px">
-      <el-form-item label="手机号">
-        <el-input v-model="user.mobile"></el-input>
+    <el-form class="login-form" ref="form" :model="user" >
+      <div class="login_head"></div>
+      <el-form-item >
+        <el-input v-model="user.mobile"
+        placeholder="请输入手机号"
+        ></el-input>
       </el-form-item>
-      <el-form-item label="验证码">
-        <el-input v-model="user.code"></el-input>
+      <el-form-item >
+        <el-input v-model="user.code"
+        placeholder="请输入验证码"
+        ></el-input>
+      </el-form-item>
+        <el-form-item >
+        <el-checkbox v-model="checked">我已阅读并同意协议和隐藏条款</el-checkbox>
       </el-form-item>
         <el-form-item>
-    <el-button type="primary" @click="onSubmit">登录</el-button>
-    <el-button>取消</el-button>
+    <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
   </el-form-item>
 </el-form>
   </div>
@@ -23,7 +30,9 @@ export default {
       user: {
         mobile: '',
         code: ''
-      }
+      },
+      checked: false,
+      imgUrl: './company_logo.png'
     }
   },
   methods: {
@@ -42,9 +51,27 @@ export default {
   right: 0;
   bottom: 0;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background: url(./download.jpg) no-repeat;
-  background-size: contain;
+  background-size: cover;
+  .login-form {
+    background-color:#fff;
+    padding: 50px;
+    min-width: 300px;
+  }
+  .login-btn {
+    width: 100%;
+  }
+  .login_head{
+    height: 57px;
+    width: 300px;
+    background: url(./company_logo.png) no-repeat;
+    padding-bottom: 30px;
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+  }
 }
 </style>
